b83466c9b9596830d28d5cb02002d2fa7861a745
Hrithik085 <hrithikpramod085@gmail.com>
2025-02-02 17:53:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import Footer from './component/Footer'
 import Vehicles from './pages/Vehicles'
 import Performance from './pages/Performance'
 import Contact from './pages/Contact'
+import Impersum from './component/Details'
 import "bootstrap/dist/css/bootstrap.min.css";
+
 
 
 
@@ -22,7 +24,9 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/vehicles' element={<Vehicles/>}/>
         <Route path='/performance' element={<Performance/>}/>
+        <Route path='/Impersum' element={<Impersum/>}/>
         <Route path='contact' element={<Contact/>}/>
+
 
 
       </Routes>
